5e37aea648882399f4bc832aea8b9469eb39c291
Dan Aukes <danaukes@seas.harvard.edu>
2015-06-11 13:48:35 -0400

    cleanup

--- build_windows.py
+++ build_windows.py
@@ -23,25 +23,14 @@ def include_entire_directory(source_dir,dest_dir):
             include.append((source,dest))
     return include
     
-iconfile = fix(popupcad.supportfiledir,'printapede.ico')
-
 popupcad_parent_directory = fix(popupcad.localpath,'../')
 
 packages = []
 packages.append('popupcad')
-packages.append('popupcad.algorithms')
-packages.append('popupcad.filetypes')
-packages.append('popupcad.geometry')
-packages.append('popupcad.graphics2d')
-packages.append('popupcad.graphics3d')
-packages.append('popupcad.manufacturing')
-packages.append('popupcad.materials')
-packages.append('popupcad.supportfiles')
-packages.append('popupcad.widgets')
-
 packages.append('dev_tools')
 packages.append('popupcad_manufacturing_plugins')
 packages.append('popupcad_deprecated')
+
 packages.append("scipy.integrate.vode")
 packages.append("scipy.integrate.lsoda")
 packages.append("scipy.sparse.csgraph._validation")
@@ -64,40 +53,10 @@ include_files.extend(include_entire_directory(fix(popupcad_parent_directory,'lic
 zip_includes = include_entire_directory(fix(python_installed_directory,"Lib\\site-packages\\OpenGL"),"OpenGL")
 
 includes = []
-#includes.append("zmq")
-#includes.append("zmq.utils.garbage")
-#includes.append("zmq.backend.cython")
-
 excludes = []
 
-msi_data = {}
-#
-#shortcut_table = [
-#    ("DesktopShortcut",        # Shortcut
-#     "DesktopFolder",          # Directory_
-#     "DTI Playlist",           # Name
-#     "TARGETDIR",              # Component_
-#     "[TARGETDIR]playlist.exe",# Target
-#     None,                     # Arguments
-#     None,                     # Description
-#     None,                     # Hotkey
-#     None,                     # Icon
-#     None,                     # IconIndex
-#     None,                     # ShowCmd
-#     'TARGETDIR'               # WkDir
-#     )
-#    ]
-
-build_exe_options = {"include_msvcr":True,"include_files":include_files,"zip_includes": zip_includes,'packages':packages,'includes':includes,'excludes':excludes,'icon':iconfile }
-
-bdist_msi_options = {
-    'upgrade_code': popupcad.windows_uuid,
-#    'add_to_path': False,
-#    'initial_target_dir': r'[ProgramFilesFolder]\%s\%s' % (company_name, product_name),
-#    'data': msi_data    
-#	"Shortcut": shortcut_table,
-
-    }
+build_exe_options = {"include_msvcr":True,"include_files":include_files,"zip_includes": zip_includes,'packages':packages,'includes':includes,'excludes':excludes,'icon':popupcad.iconfile }
+bdist_msi_options = {'upgrade_code': popupcad.windows_uuid}
 
 setup_options = {}
 setup_options['build_exe']=build_exe_options
