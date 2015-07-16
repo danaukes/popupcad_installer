eba1ad0f81e46879d9fc0a3cb02df5fa6b78c4ad
Aukes, Dan <danaukes@seas.harvard.edu>
2015-07-16 10:10:47 -0400

    added mac stuff to setup

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ from cx_Freeze import setup, Executable
 import os
 from os.path import join,normpath,dirname
 #import update_installer
+import glob
 
 def fix(*args,**kwargs):
     return normpath(join(*args,**kwargs))
@@ -35,6 +36,8 @@ packages.append('pypoly2tri')
 
 packages.append("scipy.integrate.vode")
 packages.append("scipy.integrate.lsoda")
+#if sys.platform=='darwin':
+#    packages.append('scipy.special._ufuncs_cxx')
 packages.append("scipy.sparse.csgraph._validation")
 packages.append("OpenGL.platform.win32")
 packages.append("sympy.assumptions.handlers")
@@ -45,20 +48,31 @@ packages.append("lxml._elementpath")
 
 python_installed_directory = dirname(sys.executable)
 
+zip_includes = []
 include_files = []
-include_files.append((fix(python_installed_directory,'Lib/site-packages/shapely/geos_c.dll'),'geos_c.dll'))
-include_files.append((fix(python_installed_directory,'Lib/site-packages/numpy/core/libifcoremd.dll'),'libifcoremd.dll'))
-include_files.append((fix(python_installed_directory,'Lib/site-packages/numpy/core/libifcoremd.dll'),'libifcoremd.dll'))
-include_files.append((fix(python_installed_directory,'Lib/site-packages/numpy/core/libmmd.dll'),'libmmd.dll'))
-include_files.append((fix(popupcad_parent_directory,'LICENSE'),'LICENSE'))
-include_files.extend(include_entire_directory(popupcad.supportfiledir,'supportfiles'))
-include_files.extend(include_entire_directory(popupcad.documentation_directory ,'docs'))
-include_files.extend(include_entire_directory('licenses','licenses'))
 
-zip_includes = include_entire_directory(fix(python_installed_directory,"Lib\\site-packages\\OpenGL"),"OpenGL")
+if sys.platform=='darwin':
+#    include_files.extend(glob.glob('/usr/local/Cellar/geos/3.4.2/lib/*.dylib'))
+#    include_files.extend(glob.glob('/usr/local/Cellar/geos/3.4.2/bin/lib'))
+    pass
+elif sys.platform=='win32':
+    include_files.append((fix(python_installed_directory,'Lib/site-packages/shapely/geos_c.dll'),'geos_c.dll'))
+    include_files.append((fix(python_installed_directory,'Lib/site-packages/numpy/core/libifcoremd.dll'),'libifcoremd.dll'))
+    include_files.append((fix(python_installed_directory,'Lib/site-packages/numpy/core/libifcoremd.dll'),'libifcoremd.dll'))
+    include_files.append((fix(python_installed_directory,'Lib/site-packages/numpy/core/libmmd.dll'),'libmmd.dll'))
+    include_files.append((fix(popupcad_parent_directory,'LICENSE'),'LICENSE'))
+    include_files.extend(include_entire_directory(popupcad.supportfiledir,'supportfiles'))
+    include_files.extend(include_entire_directory(popupcad.documentation_directory ,'docs'))
+    include_files.extend(include_entire_directory('licenses','licenses'))
+
+    zip_includes.extend(include_entire_directory(fix(python_installed_directory,"Lib/site-packages/OpenGL"),"OpenGL"))
+elif sys.platform=='linux':
+    pass
 
 includes = []
 excludes = []
+#if sys.platform == 'darwin':
+#    excludes.append('scipy.special._ufuncs_cxx')
 
 build_exe_options = {"include_msvcr":True,"include_files":include_files,"zip_includes": zip_includes,'packages':packages,'includes':includes,'excludes':excludes,'icon':popupcad.iconfile }
 bdist_msi_options = {'upgrade_code': popupcad.windows_uuid}
