5ba63a374cb5e981d4d0ce317c389f7b67907898
Aukes, Dan <danaukes@seas.harvard.edu>
2015-07-21 12:37:45 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -39,12 +39,17 @@ packages.append('popupcad_manufacturing_plugins')
 packages.append('popupcad_deprecated')
 packages.append('pypoly2tri')
 
+#if sys.platform=='win32':
 packages.append("scipy.integrate.vode")
 packages.append("scipy.integrate.lsoda")
 #if sys.platform=='darwin':
 #    packages.append('scipy.special._ufuncs_cxx')
 packages.append("scipy.sparse.csgraph._validation")
-packages.append("OpenGL.platform.win32")
+if sys.platform=='win32':
+    packages.append("OpenGL.platform.win32")
+elif sys.platform =='darwin':
+    packages.append("OpenGL.platform.darwin")
+    
 packages.append("sympy.assumptions.handlers")
 packages.append("numpy")
 packages.append("scipy")
@@ -59,6 +64,9 @@ include_files = []
 if sys.platform=='darwin':
 #    include_files.extend(glob.glob('/usr/local/Cellar/geos/3.4.2/lib/*.dylib'))
 #    include_files.extend(glob.glob('/usr/local/Cellar/geos/3.4.2/bin/lib'))
+    include_files.extend(include_entire_directory(popupcad.supportfiledir,'supportfiles'))
+    include_files.extend(include_entire_directory(popupcad.documentation_directory ,'docs'))
+    include_files.extend(include_entire_directory('licenses','licenses'))
     pass
 elif sys.platform=='win32':
     include_files.append((fix(python_installed_directory,'Lib/site-packages/shapely/geos_c.dll'),'geos_c.dll'))
@@ -79,12 +87,36 @@ excludes = []
 #if sys.platform == 'darwin':
 #    excludes.append('scipy.special._ufuncs_cxx')
 
-build_exe_options = {"include_msvcr":True,"include_files":include_files,"zip_includes": zip_includes,'packages':packages,'includes':includes,'excludes':excludes,'icon':popupcad.iconfile }
+build_exe_options = {}
+build_exe_options['include_msvcr']=True
+build_exe_options["include_files"]=include_files
+build_exe_options["zip_includes"]=zip_includes
+build_exe_options['packages']=packages
+build_exe_options['includes']=includes
+build_exe_options['excludes']=excludes
+build_exe_options['icon']=popupcad.iconfile
+
+bdist_mac_options = {}
+bdist_mac_options['iconfile'] = popupcad.iconfile 
+#bdist_mac_options['qt_menu_nib'] = 
+bdist_mac_options['bundle_name'] = 'popupcad_b'
+#bdist_mac_options['include_frameworks'] = []
+#bdist_mac_options['codesign_identity'] = 
+#bdist_mac_options['codesign_entitlements'] = 
+#bdist_mac_options['codesign_deep'] = 
+#bdist_mac_options['codesign_resource_rules'] = 
 bdist_msi_options = {'upgrade_code': popupcad.windows_uuid}
+
+bdist_dmg_options = {}
+bdist_dmg_options['volume_label']='popupcad_v'
+#bdist_dmg_options['applications-shortcut']=True
+#bdist_dmg_options['']=
 
 setup_options = {}
 setup_options['build_exe']=build_exe_options
 setup_options['bdist_msi']=bdist_msi_options
+setup_options['bdist_mac']=bdist_mac_options
+setup_options['bdist_dmg']=bdist_dmg_options
 
 base = None
 if sys.platform == "win32":
