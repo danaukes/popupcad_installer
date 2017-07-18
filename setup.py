b492ddaa4519dbe91e9552dbb22dc4aada93fc3c
Dan Aukes <danaukes@gmail.com>
2017-07-17 17:29:52 -0700

    updated for anaconda

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ import os
 #import glob
 import shutil
 
+import importlib
+
 # Remove the existing folders folder
 shutil.rmtree("build", ignore_errors=True)
 shutil.rmtree("dist", ignore_errors=True)
@@ -30,22 +32,20 @@ packages.append('tkinter')
 packages.append("matplotlib.backends.backend_qt4agg")
 packages.append("matplotlib.backends.backend_tkagg")
 
-#if sys.platform=='win32':
 packages.append("scipy.integrate.vode")
 packages.append("scipy.integrate.lsoda")
-#if sys.platform=='darwin':
-#    packages.append('scipy.special._ufuncs_cxx')
 packages.append("scipy.sparse.csgraph._validation")
-if sys.platform=='win32':
-    packages.append("OpenGL.platform.win32")
-elif sys.platform =='darwin':
-    packages.append("OpenGL.platform.darwin")
+packages.append("OpenGL.platform.win32")
     
 packages.append("sympy.assumptions.handlers")
 packages.append("numpy")
 packages.append("scipy")
 packages.append("lxml")
 packages.append("lxml._elementpath")
+
+packages.append('pyqtgraph')
+packages.append('OpenGL')
+packages.append('meshio')
 
 zip_includes = []
 include_files = []
@@ -54,57 +54,66 @@ include_files.extend(st.include_entire_directory(popupcad.supportfiledir,'suppor
 #include_files.extend(include_entire_directory(popupcad.documentation_directory ,'docs'))
 include_files.extend(st.include_entire_directory('licenses','licenses'))
 
-if sys.platform=='darwin':
-    pass
-elif sys.platform=='linux':
-    pass
-elif sys.platform=='win32':
-    include_files.append((st.fix(st.python_installed_directory,'Lib/site-packages/shapely/DLLs/geos_c.dll'),'geos_c.dll'))
-    include_files.append((st.fix(st.python_installed_directory,'Lib/site-packages/numpy/core/libifcoremd.dll'),'libifcoremd.dll'))
-    include_files.append((st.fix(st.python_installed_directory,'Lib/site-packages/numpy/core/libifcoremd.dll'),'libifcoremd.dll'))
-    include_files.append((st.fix(st.python_installed_directory,'Lib/site-packages/numpy/core/libmmd.dll'),'libmmd.dll'))
-    include_files.append((st.fix(popupcad_parent_directory,'LICENSE'),'LICENSE'))
+#include_files.append((st.fix(st.python_installed_directory,'Lib/site-packages/shapely/DLLs/geos_c.dll'),'geos_c.dll'))
+include_files.append((st.fix(st.python_installed_directory,'Library/bin/geos_c.dll'),'Library/bin/geos_c.dll'))
+include_files.append((st.fix(st.python_installed_directory,'Library/bin/geos.dll'),'Library/bin/geos.dll'))
+#include_files.append((st.fix(st.python_installed_directory,'Lib/site-packages/numpy/core/libifcoremd.dll'),'libifcoremd.dll'))
+#include_files.append((st.fix(st.python_installed_directory,'Lib/site-packages/numpy/core/libifcoremd.dll'),'libifcoremd.dll'))
+#include_files.append((st.fix(st.python_installed_directory,'Lib/site-packages/numpy/core/libmmd.dll'),'libmmd.dll'))
+include_files.append((st.fix(popupcad_parent_directory,'LICENSE'),'LICENSE'))
 
-    zip_includes.extend(st.include_entire_directory(st.fix(st.python_installed_directory,"Lib/site-packages/OpenGL"),"OpenGL"))
+include_files.extend(st.include_entire_directory('C:\\Miniconda3\\Library\\plugins\\platforms',''))
+include_files.extend(st.include_entire_directory('C:\\Miniconda3\\Library\\bin',''))
+
+zip_includes.extend(st.include_entire_directory(st.fix(st.python_installed_directory,"Lib/site-packages/OpenGL"),"OpenGL"))
+
+
 
 includes = []
-excludes = ['popupcad_gazebo']
+includes.append('pyqtgraph')
+includes.append('pyqtgraph.debug')
+includes.append('numpy.core._methods')
+includes.append("scipy.sparse.csgraph._validation")
+
+excludes = []
+excludes.append('popupcad_gazebo')
+
+excludes.append('gtk')
+excludes.append('_gtkagg')
+excludes.append('_tkagg')
+excludes.append('bsddb')
+excludes.append('curses')
+excludes.append('pywin.debugger')
+excludes.append('pywin.debugger.dbgcon')
+excludes.append('pywin.dialogs')
+excludes.append('tcl')
+excludes.append('tk')
+excludes.append('Tkconstants')
+excludes.append('Tkinter')
+excludes.append('babel')
+excludes.append('notebook')
+excludes.append('spyder')
+excludes.append('ipython')
+excludes.append('jupyter_client')
+excludes.append('jupyter_core')
+
 
 build_exe_options = {}
-build_exe_options['include_msvcr']=True
-build_exe_options["include_files"]=include_files
-build_exe_options["zip_includes"]=zip_includes
 build_exe_options['packages']=packages
 build_exe_options['includes']=includes
 build_exe_options['excludes']=excludes
-build_exe_options['icon']=popupcad.iconfile
-
-bdist_mac_options = {}
-bdist_mac_options['iconfile'] = popupcad.iconfile 
-#bdist_mac_options['qt_menu_nib'] = 
-bdist_mac_options['bundle_name'] = 'popupcad_bundle'
-#bdist_mac_options['include_frameworks'] = []
-#bdist_mac_options['codesign_identity'] = 
-#bdist_mac_options['codesign_entitlements'] = 
-#bdist_mac_options['codesign_deep'] = 
-#bdist_mac_options['codesign_resource_rules'] = 
+build_exe_options["include_files"]=include_files
+build_exe_options["zip_includes"]=zip_includes
+build_exe_options['include_msvcr']=True
 
 bdist_msi_options = {'upgrade_code': popupcad.windows_uuid}
 
-bdist_dmg_options = {}
-bdist_dmg_options['volume_label']='popupcad_volume'
-#bdist_dmg_options['applications-shortcut']=True
-#bdist_dmg_options['']=
 
 setup_options = {}
 setup_options['build_exe']=build_exe_options
 setup_options['bdist_msi']=bdist_msi_options
-setup_options['bdist_mac']=bdist_mac_options
-setup_options['bdist_dmg']=bdist_dmg_options
 
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
+base = "Win32GUI"
 
 setup_arguments = {}
 setup_arguments['name'] = popupcad.program_name
@@ -115,5 +124,11 @@ setup_arguments['description'] = popupcad.description
 setup_arguments['executables'] = []
 setup_arguments['executables'].append(Executable(st.fix(popupcad_parent_directory,"popupcad.py"), base=base,shortcutName=popupcad.program_name,shortcutDir="ProgramMenuFolder"))
 setup_arguments['options'] = setup_options
+
+
+module = importlib.import_module('tcl')
+p = list(module.__path__)[0]
+os.environ['TCL_LIBRARY'] = p
+os.environ['TK_LIBRARY'] = p
 
 setup(**setup_arguments)        
